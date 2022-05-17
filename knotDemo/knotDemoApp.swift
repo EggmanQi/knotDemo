72b56f37b6551a807ce785ee9133907081b5edbe
eggmanQQQ <exzero@126.com>
2022-05-17 22:52:59 +0800

    Initial Commit

---
+++ knotDemo/knotDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  knotDemoApp.swift
+//  knotDemo
+//
+//  Created by P on 2022/5/17.
+//
+
+import SwiftUI
+
+@main
+struct knotDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
